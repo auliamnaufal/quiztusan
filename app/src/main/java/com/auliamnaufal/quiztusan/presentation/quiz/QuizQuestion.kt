ab059d31c11a61614e02cdf6c29f5dc29bff03e6
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-08-17 14:14:58 +0700

    (feat): add data class for questions

---
+++ app/src/main/java/com/auliamnaufal/quiztusan/presentation/quiz/QuizQuestion.kt
@@ -0,0 +1,8 @@
+package com.auliamnaufal.quiztusan.presentation.quiz
+
+data class QuizQuestion(
+    val qustion: String,
+    val answer1: String,
+    val answer2: String,
+    val correctAnswer: Int
+)
