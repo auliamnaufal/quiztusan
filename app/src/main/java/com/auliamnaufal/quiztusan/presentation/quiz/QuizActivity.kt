78eae1fe45e4c21074cf70f83fd36125edb50ae2
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-08-17 14:09:52 +0700

    (feat): finish creating quiz screen

---
+++ app/src/main/java/com/auliamnaufal/quiztusan/presentation/quiz/QuizActivity.kt
@@ -0,0 +1,17 @@
+package com.auliamnaufal.quiztusan.presentation.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.auliamnaufal.quiztusan.databinding.ActivityMainBinding
+import com.auliamnaufal.quiztusan.databinding.ActivityQuizBinding
+
+class QuizActivity : AppCompatActivity() {
+
+    private var _binding: ActivityQuizBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
