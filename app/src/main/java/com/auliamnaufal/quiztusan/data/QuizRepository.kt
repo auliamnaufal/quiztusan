11475c300e5146bf79ace13aa18a70ef0b4fae59
ReihanFatilla <reihanfatilla@gmail.com>
2022-08-17 14:23:53 +0700

    added shared preferences to repository

--- app/src/main/java/com/auliamnaufal/quiztusan/data/QuizRepository.kt
+++ app/src/main/java/com/auliamnaufal/quiztusan/data/QuizRepository.kt
@@ -1,12 +1,23 @@
 package com.auliamnaufal.quiztusan.data
 
+import android.content.Context
+import com.auliamnaufal.quiztusan.data.sharedpref.SharedPreferences
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class QuizRepository {
-    var quizRef: FirebaseDatabase = FirebaseDatabase.getInstance()
+class QuizRepository(context: Context) {
+    private var quizRef: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private var pref = SharedPreferences(context)
 
     fun getReference(ref: String): DatabaseReference {
         return quizRef.getReference(ref)
+    }
+
+    fun add(key: String, value: String){
+        pref.add(key, value)
+    }
+
+    fun getString(key: String): String? {
+        return pref.getString(key)
     }
 }
